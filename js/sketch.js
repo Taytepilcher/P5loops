cf38f6fdf5c197f56ce19a49b3f7ec84b3a12ed6
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2022-11-25 10:54:24 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,39 @@
+var myarray=["5","4","3","2","1"]
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    for (let i = 0; i<10; i++){
+        console.log("I want "+i+ " Ice cream");
+    }
+
+
+    for(let i =0; i<myarray.length;i++){
+console.log(myarray[i])
+    }
+    
+
 }
 
 function draw() {
     background(220);
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    
+    
+    //for(let lineX =80;lineX<=width; lineX+=80 ){
+      //  line(lineX, 0,lineX, height)
+    //}
+    //for(let x = 0 ; x<=30; x++){
+    //    ellipse(x*10, height/2, 10);
+   // }
+   for(let y = 0 ; y<=80; y++)
+   {
+       for(let x = 0; x <=80;x++)
+       {
+        fill(x*y,x*10,y);
+circle(x*10, y*10, 10)
+       }
+   }
+
 }
